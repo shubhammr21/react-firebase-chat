031db6258004145bba244bf87051369e573de337
shubhammr21 <shubhammr21@gmail.com>
2024-08-26 14:06:49 +0530

    feat(chat-list): implement search and create chat functionality
    Summary:
    - Add search bar functionality to the ChatList component
    - Add button to toggle create chat mode
    
    The ChatList component now includes:
    
    - A search bar with an input field and search icon
    - A button that toggles between "+" and "-" icons, allowing the user to enter or exit the create chat mode
    
    By adding these features, users can search for chats and initiate the creation of a new chat. The search bar input is styled to blend with the app's color scheme, and the toggle button provides a clear visual cue for the create chat mode.
    
    These enhancements improve the user experience by enabling easy navigation and chat management within the application.

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,7 +1,24 @@
+import { useState } from "react"
 import "./chatList.css"
 
 function ChatList() {
-  return <div className="chatList">ChatList</div>
+  const [addMode, setAddMode] = useState(false)
+  return (
+    <div className="chatList">
+      <div className="search">
+        <div className="searchBar">
+          <img src="./search.png" alt="search" />
+          <input type="text" name="search" id="search" placeholder="Search" />
+        </div>
+        <img
+          src={addMode ? "./minus.png" : "./plus.png"}
+          alt="create chat"
+          className="add"
+          onClick={() => setAddMode(prev => !prev)}
+        />
+      </div>
+    </div>
+  )
 }
 
 export default ChatList
