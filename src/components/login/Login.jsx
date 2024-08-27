8ba16bf18115a0bd3db6f8a2b0f1d2fe04efbe5c
shubhammr21 <shubhammr21@gmail.com>
2024-08-27 13:49:57 +0530

    feat(auth): add user registration functionality
    - Add form and input fields for new user registration
    - Implement handleRegister function to create new user with email and password
    - Store user data (username, email, id, blocked) in Firestore database
    - Display success or error toast notification based on the registration outcome
    
    This commit introduces user registration functionality, allowing new users to create an account by providing a username, email, and password. The registration process involves creating a new user authentication entry in Firebase Authentication and storing the user's data in the Firestore database. Upon successful registration, a success toast notification is shown, while errors are displayed with an error toast message. This new feature enhances the application by enabling user onboarding and account creation.

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,5 +1,8 @@
+import { createUserWithEmailAndPassword } from "firebase/auth"
+import { doc, setDoc } from "firebase/firestore/lite"
 import { useState } from "react"
 import { toast } from "react-toastify"
+import { auth, db } from "../../lib/firebase"
 import "./login.css"
 function Login() {
   const [avatar, setAvatar] = useState({
@@ -17,9 +20,38 @@ function Login() {
     }
   }
 
-  const handleLogin = e => {
+  const handleLogin = async e => {
     e.preventDefault()
+
     toast.success("Hello")
+  }
+  const handleRegister = async e => {
+    e.preventDefault()
+    const formData = new FormData(e.target)
+    const { username, email, password } = Object.fromEntries(formData)
+    if (!(username || email || password)) {
+      toast.warn("All field required")
+
+      return
+    }
+
+    try {
+      const response = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      )
+      await setDoc(doc(db, "users", response.user.uid), {
+        username,
+        email,
+        id: response.user.uid,
+        blocked: []
+      })
+      toast.success("Account created! You can login now!")
+    } catch (error) {
+      console.log(error)
+      toast.error(error.message)
+    }
   }
 
   return (
@@ -35,7 +67,7 @@ function Login() {
       <div className="separator"></div>
       <div className="item">
         <h2>Create an Account</h2>
-        <form>
+        <form onSubmit={handleRegister}>
           <label htmlFor="avatar">
             <img src={avatar.url || "./avatar.png"} alt="avatar" />
             Upload an image
@@ -47,9 +79,14 @@ function Login() {
             style={{ display: "none" }}
             onChange={handleAvatar}
           />
-          <input type="text" name="username" placeholder="Username" />
-          <input type="text" name="email" placeholder="Email" />
-          <input type="password" name="password" placeholder="Password" />
+          <input type="text" name="username" placeholder="Username" required />
+          <input type="text" name="email" placeholder="Email" required />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            required
+          />
           <button>Sign up</button>
         </form>
       </div>
