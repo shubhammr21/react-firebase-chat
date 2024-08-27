40fcfab538a01fe6274e393be33da56a0a27a3b6
shubhammr21 <shubhammr21@gmail.com>
2024-08-27 13:23:21 +0530

    feat(firebase): integrate firebase services
    - Add Firebase configuration files (.firebaserc, firebase.json)
    - Set up Firebase Authentication, Firestore, and Storage rules
    - Initialize Firebase app with environment variables
    - Connect to Firebase emulators in development mode
    - Add firebase npm package as a dependency
    
    The changes aim to integrate Firebase services (Authentication, Firestore, and Storage) into the application. This allows for features like user authentication, real-time database management, and file storage. The Firebase configuration files and rules are added, and the necessary Firebase services are initialized and connected to the app, with the ability to use emulators during development for testing purposes.

---
+++ src/lib/firebase.js
@@ -0,0 +1,36 @@
+import { initializeApp } from "firebase/app"
+import { connectAuthEmulator, getAuth } from "firebase/auth"
+import { connectFirestoreEmulator, getFirestore } from "firebase/firestore/lite"
+import { connectStorageEmulator, getStorage } from "firebase/storage"
+
+let auth
+let db
+let storage
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
+}
+const app = initializeApp(firebaseConfig)
+
+if (import.meta.env.MODE === "development") {
+  auth = getAuth()
+  db = getFirestore()
+  storage = getStorage()
+
+  connectAuthEmulator(auth, "http://localhost:9099")
+  connectFirestoreEmulator(db, "localhost", 9299)
+  connectStorageEmulator(storage, "localhost", 9199)
+  //
+} else {
+  // Initialize Firebase
+  auth = getAuth(app)
+  db = getFirestore(app)
+  storage = getStorage(app)
+}
+
+export { auth, db, storage }
